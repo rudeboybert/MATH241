9a7b2ec8ccc2458719e97a17c3a6ac7a26310de8
Albert Y. Kim <albert.ys.kim@gmail.com>
2015-01-29 19:24:38 -0800

    Started Lecture 03: Grammar for Data Manipulation

---
+++ Lec03 dplyr/dplyr.R
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------
+# The following code ensures all necessary packages are installed
+#------------------------------------------------------------------------------
+pkg <- c("ggplot2", "dplyr")
+new.pkg <- pkg[!(pkg %in% installed.packages())]
+if (length(new.pkg)) {
+  install.packages(new.pkg, repos="http://cran.rstudio.com/")
+}
+library(ggplot2)
+library(dplyr)
