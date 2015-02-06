3e2bc263fec8db9d7a3f2201d52ef56b071c9586
Albert Y. Kim <albert.ys.kim@gmail.com>
2015-02-06 12:08:51 -0800

    Almost done Lec06: HW01

--- Lec06 R Markdown + HW01/HW01.R
+++ Lec06 R Markdown + HW01/HW01.R
@@ -1,13 +1,148 @@
+#------------------------------------------------------------------------------
+# Lines 1:77 are from last time:
+# Make sure the directory is set to where states.csv is
+# Load all necessary packages and recompute Washington Post data
+#------------------------------------------------------------------------------
+# Install necessary packages
+pkg <- c("devtools", "rvest", "ggthemes",  "dplyr", "ggplot2", "ggmap",
+         "RColorBrewer", "htmlwidgets", "RCurl", "scales")
+
+new.pkg <- pkg[!(pkg %in% installed.packages())]
+if (length(new.pkg)) {
+  install.packages(new.pkg)
+}
+# Install rvest and leaflet packages
+if (!require("rvest")) {
+  devtools::install_github("hadley/rvest")
+}
+if (!require("leaflet")) {
+  devtools::install_github("rstudio/leaflet")
+}
+
+# Load all packages
+library(rvest)
+library(leaflet)
+library(devtools)
+library(htmlwidgets)
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
+library(ggmap)
+library(RColorBrewer)
+library(magrittr)
+library(RCurl)
+library(scales)
+
+# Load data
+webpage <- html("http://apps.washingtonpost.com/g/page/local/college-grants-for-the-affluent/1526/")
+wp_data <- webpage %>% html_nodes("table") %>% .[[1]] %>% html_table()
+
+# Load functions
+clean.names <- function(df){
+  colnames(df) <- gsub("[^[:alnum:]]", "", colnames(df))
+  colnames(df) <- tolower(colnames(df))
+  return(df)
+}
+currency.to.numeric <- function(x){
+  x <- gsub('\\$','', as.character(x))
+  x <- gsub('\\,','', as.character(x))
+  x <- as.numeric(x)
+  return(x)
+}
+
+# Clean data
+wp_data <-
+  clean.names(wp_data) %>%
+  rename(
+    comp_fee = tuitionfeesroomandboard201314,
+    p_no_need_grant = percentoffreshmenreceivingnoneedgrantsfromschool,
+    ave_no_need_grant = averagenoneedawardtofreshmen,
+    p_need_grant = percentreceivingneedbasedgrantsfromschool
+  ) %>%
+  mutate(
+    state = as.factor(state),
+    sector = as.factor(sector),
+    comp_fee = currency.to.numeric(comp_fee),
+    ave_no_need_grant = currency.to.numeric(ave_no_need_grant),
+    p_no_need_grant = ifelse(is.na(p_no_need_grant), .5, p_no_need_grant)
+  ) %>%
+  tbl_df()
+
+state.means <- wp_data %>%
+  select(state, ave_no_need_grant) %>%
+  group_by(state) %>%
+  summarise_each(funs(mean)) %>%
+  arrange(desc(ave_no_need_grant))
+
+state.info <- read.csv("states.csv", header=TRUE) %>% tbl_df()
+
+
+
+
+
+#------------------------------------------------------------------------------
+# EXERCISE FROM LAST TIME:
+# 1. Merge the state.means data with the new state.info data so that we know
+# what region each observation (i.e. university) is in
+
+# ANSWER:  I use left_join here.  We only want to append info that exists in
+# state.means
 state.means <- left_join(state.means, state.info, by="state")
 state.means
-# Now we replot the plot from above, but with the fill aesthetic set to "region"
+
+
+
+# 2. Recreate the bar chart from above, but color code the bars by which region
+# of the US the state is a member of NE, south, west, midwest.  What trend do
+# you notice?
+
+# ANSWER:  We add fill=region (color=region) only does the outlines
 ggplot(data=state.means, aes(x=state, y=ave_no_need_grant, fill=region)) +
   geom_bar(stat="identity") +
   ylab("avg no need grant")
 
 
 
+# Somebody asked how do we rearrange the ordering of the states on the x-axis.
+# Unfortunately there doesn't seem to be a clean dplyr way to do this.  Here is
+# the hacky solution.  We first arrange the state.means data.frame in the order
+# we want the states to show:
+state.means <- arrange(state.means, region, desc(ave_no_need_grant))
+state.means
 
+# Then we "factor" the state variable to be a categorial variable whose
+# "levels" determine the order.  If no levels are specified, then alphabetical
+# is the default
+state.means$state <- factor(state.means$state, levels = state.means$state)
+state.means$state
+
+# Now we plot
+ggplot(data=state.means, aes(x=state, y=ave_no_need_grant, fill=region)) +
+  geom_bar(stat="identity") +
+  ylab("avg no need grant")
+
+
+
+# EXERCISE:  By changing the left_join() function to another join function,
+# change the plot so that states that do not have any data do not get plotted at
+# at all
+
+# ANSWER:  We use inner_join instead() of left_join(), that way only states
+# that exist in both data frames are plotted
+state.data <- map_data("state") %>% tbl_df()
+state.data <- inner_join(state.data, state.means, by=c("region" = "fullname"))
+
+ggplot(state.data, aes(long, lat, group = group)) + # Recall "grouping"
+  geom_polygon(aes(fill = ave_no_need_grant)) +
+  geom_path(color="white") + # outline of states
+  scale_fill_gradient(name="Avg. No\nNeed Grant", low='white', high='red')
+
+
+
+# EXERCISE:  Recreate the above barchart and map but now split by the sector of the
+# university: public or private.
+
+# ANSWER:  We also group_by() sector on top of state:
 state.means <- wp_data %>%
   select(state, sector, ave_no_need_grant) %>%
   group_by(state, sector) %>%
@@ -24,24 +159,8 @@ ggplot(data=state.means, aes(x=state, y=ave_no_need_grant, fill=region)) +
 
 
 
-# We join the state.means data and the map_data corresponding to states
-state.data <- map_data("state") %>%
-  inner_join(state.means, by=c("region" = "fullname")) %>%
-  tbl_df()
-state.data
-
-# What do you think the grey values mean? Note:  the "\n" is the "return"
-# character
-ggplot(state.data, aes(long, lat, group = group)) +
-  geom_polygon(aes(fill = ave_no_need_grant)) +
-  geom_path(color="white") +
-  scale_fill_gradient(name="Avg. No\nNeed Grant", low='white', high='red')
-
-
-
-
 #------------------------------------------------------------------------------
-# Using the join command
+# Homework Data
 #------------------------------------------------------------------------------
 flights <- read.csv("flights.csv", stringsAsFactors = FALSE) %>%
   tbl_df() %>%
--- Lec06 R Markdown + HW01/R_markdown_HW_01.md
+++ Lec06 R Markdown + HW01/R_markdown_HW_01.md
@@ -8,15 +8,59 @@ date: Friday 2015/02/06
 
 
 
+Questions
+========================================================
+
+Last time someone asked how to reorder the bars on the barplot.  We do that by setting the `state` variable as a factor, i.e. a categorical variable, and then setting its order via its `levels`.
+
+See lines 106-120.
+
+
+
+A few other commands
+========================================================
+
+* `count()`.  Ex:
+    + `count(diamonds, color)`
+    + `count(diamonds, color, clarity)`
+* `summarise(n())`.  Ex:
+    + `group_by(diamonds, color) %>% summarise(n())`
+* How to deal with missing data
+    + `mean(c(1, 2, NA))` gives `NA`
+    + but `mean(c(1, 2, NA), na.rm=TRUE)` gives 1.5.
+
+
+
 Flights data
 ========================================================
 
-* `flights` [227,496 x 14]. Every flight departing Houston in 2011.
+* `flights` [227,496 x 14]. Every domestic flight departing Houston in 2011.
 * `weather` [8,723 x 14]. Hourly weather data.
 * `planes` [2,853 x 9]. Plane metadata.
 * `airports` [3,376 x 7]. Airport metadata.
 
-Source: RStudio
+Source: RStudio.
+
+
+
+Questions for Homework
+========================================================
+
+* Plot a "time series" of the number of flights that were delayed by more than 30 minutes on each day.  i.e.
+    + the x-axis should be some notion of time
+    + the y-axis should be the proportion.
+* Which seasons did we tend to see the most and least delays of > 30 minutes.
+* I actually prefer flying on older planes.  Even though they aren't as nice, they tend to have more room.  Which airlines should I favor?
+* What states did Southwest Airlines flights tend to fly to from Houston?
+
+
+
+Questions for Homework
+========================================================
+
+* What weather patterns are associated with the biggest departure delays?
+* I want to know what regions (NE, south, west, midwest) each carrier flies to from Houston.
+* Get creative:  Answer one question of your own using this data based on your feelings on flying.  The more interesting the question the better.
 
 
 
@@ -29,10 +73,16 @@ Strategy
     + I want to do Y, then
     + ...
 * Once you have the appropriate **tidy** data frame sketch out what you want your graphic to look like and build it using the **grammar of graphics**
-* For those of you with no programming experience: if you're spinning your wheels, TAKE A BREAK.
 
 
 
+
+Strategy
+========================================================
+
+* For those of you with no programming experience: if you're spinning your wheels, TAKE A BREAK.  If really stuck, get help from your peers, me, or Rich.
+* There are no "right" answers.  Just think of the goal:  you want to convey the most information so that consumers need to use the least cerebral power to understand it.
+* [Grading Rubric](http://stat545-ubc.github.io/peer-review01_marking-rubric.html)
 
 
 
@@ -42,10 +92,3 @@ R Markdown
 
 
 
-
-
-Homeworks
-========================================================
-
-*
-* [Grading Rubric](http://stat545-ubc.github.io/peer-review01_marking-rubric.html)
