22bb2d753f480aeda0bb40140f9ac9c1cd877889
Albert Y. Kim <albert.ys.kim@gmail.com>
2015-03-06 12:22:51 -0800

    Done Lec18

--- Lec18 HW03/lubridate_solutions.R
+++ Lec18 HW03/lubridate_solutions.R
@@ -19,39 +19,8 @@ bitcoin <- rename(bitcoin, Avg = `24h Average`, Total.Volume = `Total Volume`)
 
 
 #-------------------------------------------------------------------------------
-# Exercise for today
-#-------------------------------------------------------------------------------
-# Run this line, search for "lubridate", and click on HTML to get the "vignette"
-# (example file).  You can also browse the source code.
-browseVignettes()
-
-# Go over the source code, but for each section, I've added some additional
-# notes.
-
-
-
-#-------------------------------------------------------------------------------
 # Parsing dates and times
 #-------------------------------------------------------------------------------
-arrive <- ymd_hms("2011-03-04 12:00:00", tz = "Pacific/Auckland")
-leave <- ymd_hms("2011-08-10 14:00:00", tz = "Pacific/Auckland")
-
-# ymd_hms() is a cookie-cutter function.  parse_date_time() allows more
-# flexible parsing of Date/Time Objects than ymd_hms(), but requires more
-# careful specification.  Look at the help file and look at Details section
-?parse_date_time()
-
-# Example say we have a date/time object that's not "clean"
-x <- "Sun 2003 Nov 30 19:48:20"
-parse_date_time(x, "%Y %b %d %H%M%S")
-
-x <- "Sunday 03 Nov 30 19:48:20"
-parse_date_time(x, "%y %b %d %H%M%S")
-
-# lubridate knows to ignore weekday name.  Look at the list of formats in the
-# help file to get a sense for how this function parses the date.
-
-
 # EXERCISE: As it is, the dates in the bitcoin data are simply character
 # strings.  Convert the date variable from character strings to "POSIXct"
 # date/time objects and plot a time series of the avg price of bitcoins relative
@@ -72,26 +41,6 @@ p + geom_smooth()
 #-------------------------------------------------------------------------------
 # Setting and Extracting information
 #-------------------------------------------------------------------------------
-# As covered in the Vignette:
-second(arrive)
-minute(arrive)
-hour(arrive)
-day(arrive)
-month(arrive)
-year(arrive)
-wday(arrive)
-wday(arrive, label = TRUE)
-week(arrive)
-
-# Extra:  Change individual components
-day(arrive) <- day(arrive) + 365
-arrive
-
-# Change it back
-month(arrive) <- month(arrive) - 12
-arrive
-
-
 # EXERCISE: Create a new variable day.of.week which specifies the day of week
 # and compute the mean trading value split by day of week.  Which day of the
 # week is there on average the most trading of bitcoins?
@@ -102,55 +51,15 @@ group_by(bitcoin, day.of.week) %>%
 
 
 
-#-------------------------------------------------------------------------------
-# Time Zones
-#-------------------------------------------------------------------------------
-# This list all time zones:
-OlsonNames()
-
-# Hadley Wickham is from New Zealand, so he sets a Skype meeting for NZ time
-meeting <- ymd_hms("2011-07-01 09:00:00", tz = "Pacific/Auckland")
-meeting
-
-# What time is this in Portland?
-with_tz(meeting, "America/Los_Angeles")
-
-# Let's change the time zone to ours permanently
-meeting <- force_tz(meeting, "America/Los_Angeles")
-meeting
-
-
-
-#-------------------------------------------------------------------------------
-# Time Intervals
-#-------------------------------------------------------------------------------
-auckland <- interval(arrive, leave) # Same as arrive %--% leave
-jsm <- interval(ymd(20110720, tz = "Pacific/Auckland"), ymd(20110831, tz = "Pacific/Auckland"))
-
-# Compare these two time intervals.  A bit hard to read unfortunately
-auckland
-jsm
-
-
-
 #---------------------------------------------------------------
 # Interval functions
 #---------------------------------------------------------------
-int_overlaps(jsm, auckland)
-int_start(jsm)
-int_flip(jsm)
-int_shift(jsm, new_duration(week=12))
-
-x <- c(ymd(20110725, tz = "Pacific/Auckland"), ymd(20110901, tz = "Pacific/Auckland"))
-x
-x %within% jsm
-
-
 # EXERCISE:  Using the interval and %within% commands, plot the times series
 # for the price of bitcoin to dates in 2013 and on.
 date.range <- interval(ymd(20130101), ymd(20150601))
-filter(bitcoin, Date %within% date.range) %>%
-  ggplot(data=., aes(x=Date, y=Avg)) + geom_line() +
+bitcoin <- filter(bitcoin, Date %within% date.range)
+
+ggplot(data=bitcoin, aes(x=Date, y=Avg)) + geom_line() +
   xlab("Date") + ylab("Bitcoin price vs US Dollar") + geom_smooth()
 
 # EXERCISE.  Replot the above curve so that the 4 seasons are in different
@@ -161,15 +70,28 @@ filter(bitcoin, Date %within% date.range) %>%
 # similar to %within% but for individual elements, not intervals.
 c(3,5) %in% c(1,2,3)
 
+
+# This gets a bit nasty in the second mutate below:  we have a series of nested
+# ifelse() statements
 bitcoin <-
-  filter(bitcoin, Date %within% date.range) %>%
-  mutate(month=month(Date),
-       Season = ifelse(month %in% c(1,2,3), "spring",
-                       ifelse(month %in% c(4,5,6), "summer",
-                              ifelse(month %in% c(7,8,9), "fall", "winter")
-                       )),
-       Season = factor(Season, levels=c("spring", "summer", "fall", "winter"))
-       )
+  mutate(bitcoin, month=month(Date)) %>%
+  mutate(
+         Season = ifelse(month %in% c(1,2,3), "spring",
+                         ifelse(month %in% c(4,5,6), "summer",
+                                ifelse(month %in% c(7,8,9), "fall", "winter")
+                         ))
+  ) %>%
+
+# We could alternatively using piping, but needs to be outside the dplyr piping
+# or else things will get confused
+bitcoin$Season <-
+  ifelse(bitcoin$month %in% c(7,8,9), "fall", "winter") %>%
+  ifelse(bitcoin$month %in% c(4,5,6), "summer", .) %>%
+  ifelse(bitcoin$month %in% c(1,2,3), "spring", .)
+
+
+# Reorder the factors
+bitcoin$Season <- factor(bitcoin$Season, levels=c("spring", "summer", "fall", "winter"))
 
 ggplot(data=bitcoin, aes(x=Date, y=Avg)) +
   geom_point(aes(col=Season)) +
--- Lec18 HW03/preprocess.R
+++ Lec18 HW03/preprocess.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 
-# Read in raw data, clean-up filenames, and count the number of slashes.
-# If there are
+# Read in raw data, clean-up filenames, and count the number of slashes. If
+# there are
 # -3 slashes, that means where was no subfolder with the album name
 # -4 slashes, that means where was a subfolder with the album name
 # We remove a few cases that had 2 or 5 slashes, i.e. weird formatting of the
@@ -17,7 +17,8 @@ jukebox <- read.csv("mp3log.csv", header=FALSE) %>% tbl_df() %>%
     ) %>%
   filter(num.slash %in% c(3, 4))
 
-# Split each filename at each "/".  The data is stored in a "list"
+# Split each filename at each "/".  The data is stored in a list (similar to a
+# vector)
 filename <- str_split(jukebox$file, "/")
 
 # The artist is always the 2nd element of each list, the album is either the
@@ -30,4 +31,5 @@ jukebox <-
   ) %>%
   select(-file, -num.slash)
 
+# Write to a new CSV file
 write.csv(jukebox, "jukebox.csv", row.names=FALSE)
